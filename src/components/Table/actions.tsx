f4014d479e42ad9a7a20ed887fe92b29aa2e977e
Matias Barra <matias.barra@alumnos.uach.cl>
2023-10-30 02:01:00 -0300

    fix row options and modal

--- src/components/Table/actions.tsx
+++ src/components/Table/actions.tsx
@@ -1,27 +1,58 @@
 "use client"
 
 import { MoreHorizontal } from "lucide-react";
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { type sheetCols } from "@/lib/types";
 
 
 export default function ActionsColum({ }) {
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                    <span className="sr-only">Open menu</span>
-                    <MoreHorizontal className="h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Editar</DropdownMenuItem>
-                <DropdownMenuItem>Ver cliente</DropdownMenuItem>
-                <DropdownMenuItem>Ver detalles del pago</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" >
+                        <MoreHorizontal className="h-5 w-5" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        Editar
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Ver detalles
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="text-destructive" onClick={() => setIsEditDialogOpen(true)}>
+                        Eliminar
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Dialog
+                open={isEditDialogOpen}
+            >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>¿Estás seguro?</DialogTitle>
+                        <DialogDescription>
+                            ¿Quieres eliminar la entrada? Eliminar esta entrada no se puede
+                            deshacer.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <DialogClose asChild onClick={() => setIsEditDialogOpen(false)}>
+                            <Button variant="outline">Cancelar</Button>
+                        </DialogClose>
+                        <Button>Eliminar entrada</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
     )
 }
+
